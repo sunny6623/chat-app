3e18086c94505bed6933f9c808c9ccf0798bec21
Lokesh Putta <putta.lokesh@agreeya.net>
2024-01-27 10:33:02 +0530

    settingup socket.io basic connection

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
-const path = require("path"); // Add this line
+const path = require("path");
+const http = require("http");
+const sockerio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = sockerio(server);
 
 const port = process.env.port || 3000; // Corrected the port variable name
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
-    console.log("Server is up on port " + port);
+io.on("connection", () => {
+  console.log("new websocket connection");
+});
+
+server.listen(port, () => {
+  console.log("Server is up on port " + port);
 });
